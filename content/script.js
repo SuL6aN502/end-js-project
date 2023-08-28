169fe1b31c297b95507503544fce6e829a15ca9b
سلطان <gbj6420@gmail.com>
2023-08-29 00:17:28 +0300

    start

---
+++ content/script.js
@@ -0,0 +1,12 @@
+let body = document.body
+let main = document.createElement('main')
+let sect_1 = document.createElement('section')
+let sect_2 = document.createElement('section')
+let sect_3 = document.createElement('section')
+body.appendChild(main)
+main.appendChild(sect_1)
+main.appendChild(sect_2)
+main.appendChild(sect_3)
+sect_1.className = 'sect-1'
+sect_2.className = 'sect-2'
+sect_3.className = 'sect-3'
